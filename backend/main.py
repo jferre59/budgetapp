0edce7fefe66468e143713f9b0641d54965569ed
Jacob Ferreira <jferre59@my.centennialcollege.ca>
2026-06-17 18:16:53 -0400

    Set up Alembic

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-from database import engine, Base
-from models.transactions import Transaction
+#from database import engine, Base
+#from models.transactions import Transaction
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
